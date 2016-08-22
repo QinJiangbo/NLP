4fedefc6fafcfb42462ae7e5828df225849ef1b0
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2016-08-22 22:57:34 +0800

    remove main function as test entry

--- src/main/java/com/qinjiangbo/service/BaiDuVoiceTest.java
+++ src/main/java/com/qinjiangbo/service/BaiDuVoiceTest.java
@@ -18,7 +18,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLContextBuilder;
